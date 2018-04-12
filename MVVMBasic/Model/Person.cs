33d175dce4c420f6e90ef187aa03239f95b1e497
sachin.vishwakarma <sachin.vishwakarma@DEL1-DHP-32427>
2018-04-12 16:47:24 +0530

    Add project files.

---
+++ MVVMBasic/Model/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+
+namespace MVVMBasic.Model
+{
+    class Person : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+                OnPropertyChanged("Fullname");
+            }
+        }
+
+        private string _lastname;
+        public string Lastname
+        {
+            get { return _lastname; }
+            set
+            {
+                _lastname = value;
+                OnPropertyChanged("Lastname");
+                OnPropertyChanged("Fullname");
+            }
+        }
+        private string _fullname;
+        public string Fullname
+        {
+            get { return _name + " " + _lastname; }
+            set
+            {
+                _fullname = value;
+                OnPropertyChanged("Fullname");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Person()
+        {
+            if (DesignMode.DesignModeEnabled)
+            {
+                this.Name = "sachin";
+                this.Lastname = "vishwakarma";
+            }
+        }
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ MVVMBasic/ViewModel/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using MVVMBasic.ViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMBasic.ViewModel
+{
+   public class ViewModelBase
+    {
+        public SimpleCommand SimpleCommand { get; set; }
+        public ViewModelBase()
+        {
+            this.SimpleCommand = new SimpleCommand(this);
+        }
+
+        public void SimpleMethod()
+        {
+            Debug.WriteLine("hello");
+        }
+    }
+}
